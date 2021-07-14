7a7f0ef34b778c990f22d119eb80e328ae731f99
Junalynn Caoli <junalynn14@gmail.com>
2021-07-13 22:59:50 -0700

    TodoInput

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,9 +5,15 @@ class TodoInput extends Component {
 
     constructor(props) {
         super(props);
+        this.state = { value: '' };
 
+        this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
 
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
     }
 
     handleSubmit(event) {
@@ -16,9 +22,10 @@ class TodoInput extends Component {
 
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <label>
                     What do you need to do today?
+                    <input type="text" value={this.state.value} onChange={this.handleChange} />
                 </label>
                 <input type="submit" value="Submit" />
             </form>
@@ -28,10 +35,6 @@ class TodoInput extends Component {
 
 
 class Main extends Component {
-
-    constructor(props) {
-        super(props);
-    }
 
     render() {
         return (
